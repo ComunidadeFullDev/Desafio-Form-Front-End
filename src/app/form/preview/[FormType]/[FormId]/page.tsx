1f5097cfbf8a9e9a6dd5f062d06b14bf5a98159e
Eric <ericsouza94895@gmail.com>
2025-01-18 00:53:44 -0300

    feat: add parametros na função PreviewForm para que aceite links dinâmicos

--- src/app/form/preview/[FormType]/[FormId]/page.tsx
+++ src/app/form/preview/[FormType]/[FormId]/page.tsx
@@ -1,10 +1,10 @@
 import FormPreview from "@/components/FormPreview";
 
-export default function PreviewForm () {
-    
+export default function PreviewForm({params}: {params: { FormId: string; FormType: string };}) {
+
     return (
         <div>
-            <FormPreview />
-                    </div>
+            <FormPreview formType={params.FormType} formId={params.FormId} />
+        </div>
     )
 }
